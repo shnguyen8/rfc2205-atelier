a3a5df5da16bb7de49014d19d7e0a9088bb90493
Jean Kim <jeankim119@gmail.com>
2022-07-08 15:34:45 -0500

    API changes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const handlers = require('./handlers.js')
 app.use(express.json());
 
 
 app.use(express.static('client/dist'))
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT);
 console.log(`Server listening at http://localhost:${PORT}`);
+
+
+app.get('/:word', function(req, res) {
+  handlers.getInfo(req.url)
+  .then((data) => {
+    console.log(data);
+    res.status(200).json(data.data);
+  })
+  .catch((err) => {
+    res.status(500).json({ message: 'Internal Server Error'})
+  })
+})
