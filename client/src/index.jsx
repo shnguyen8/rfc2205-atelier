9c8fc1ee0c530a4339f42f2c9e1777913fbe03d2
Dylan <dylanphollier@gmail.com>
2022-07-13 10:56:31 -0500

    Promise.all for get requests in index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -15,55 +15,40 @@ class App extends React.Component {
       initialReviews: [],
       metaData: {},
       productStyles: {},
-      relatedProducts: []
+      relatedProducts: [],
+      allProducts: [],
     }
   }
 
   componentDidMount = () => {
-    this.getProducts();
-    this.getAllData();
+    this.getMetaData();
+    this.getAllDataPromise();
   }
-
 
   getAllData = () => {
-    this.getProductInfo();
-    this.getInitialReviews();
-    this.getMetaData();
-    this.getProductStyles();
-    this.getRelatedProducts();
+    this.getAllDataPromise();
   }
 
-  getProducts = () => {
-    axios.get('/products')
-      .then(res => {
-        // console.log(res.data);
-        this.setState({
-          products: res.data,
-          currentProduct: res.data[0]['id']
-        })
-        return res.data;
-      })
-    .catch(err => {console.log(err)})
-  }
-
-  getProductInfo = () => {
-    axios.get(`products/${this.state.currentProduct}`)
-      .then(res => {
-        this.setState({
-          productSpecs: res.data,
-        })
-      })
-      .catch(err => {console.log(err)})
-
-  }
-
-  getInitialReviews = () => {
+  getAllDataPromise = async () => {
     let params = {product_id: this.state.currentProduct, page: 1}
-    axios.get('/reviews', {params})
-    .then(res => {this.setState({
-      initialReviews: res.data
-    })})
-    .catch(err => {console.log(err)})
+    await Promise.all([
+      axios.get('/products'),
+      axios.get(`products/${this.state.currentProduct}`),
+      axios.get('/reviews', {params}),
+      axios.get(`products/${this.state.currentProduct}/styles`),
+      axios.get(`products/${this.state.currentProduct}/related`),
+      axios.get("/products/?count=5000")
+    ])
+    .then((res) => {
+      this.setState({
+        products: res[0].data,
+        productSpecs: res[1].data,
+        initialReviews: res[2].data,
+        productStyles: res[3].data,
+        relatedProducts: res[4].data,
+        allProducts: res[5].data
+      })
+    })
   }
 
   getMetaData = () => {
@@ -74,29 +59,6 @@ class App extends React.Component {
     })})
     .catch(err => {console.log(err)})
   }
-
-  getProductStyles = () => {
-    axios.get(`products/${this.state.currentProduct}/styles`)
-      .then(res => {
-        this.setState({
-          productStyles: res.data,
-        })
-      })
-      .catch(err => {console.log(err)})
-
-  }
-
-  getRelatedProducts = () => {
-    axios.get(`products/${this.state.currentProduct}/related`)
-      .then(res => {
-        this.setState({
-          relatedProducts: res.data,
-        })
-      })
-      .catch(err => {console.log(err)})
-
-  }
-
 
   onChange = (event) => {
     this.setState({
@@ -116,21 +78,20 @@ class App extends React.Component {
         />
         <button onClick={this.getAllData} >Submit</button>
         <ProdOverview
-          currentProduct= {this.state.currentProduct}
-          products={this.state.products}
-          productSpecs={this.state.productSpecs}
-          productStyles={this.state.productStyles}
+          currentProduct = {this.state.currentProduct}
+          products = {this.state.products}
+          productSpecs = {this.state.productSpecs}
+          productStyles = {this.state.productStyles}
         />
         <Ratings
-          currentProduct= {this.state.currentProduct}
+          currentProduct = {this.state.currentProduct}
           initialReviews = {this.state.initialReviews}
           metaData = {this.state.metaData}
         />
         <Related
-          products={this.state.products}
-          currentProduct= {this.state.currentProduct}
-          productStyles={this.state.productStyles}
+          currentProduct = {this.state.currentProduct}
           relatedProducts = {this.state.relatedProducts}
+          allProducts = {this.state.allProducts}
         />
       </div>
     )
